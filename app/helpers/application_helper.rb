293311ba80fceb6f83dab10fafe9d1aa0a4a3b54
Rei <chloerei@gmail.com>
2011-10-05 17:50:24 +0800

    Add person link.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,13 @@ module ApplicationHelper
                                        :autolink => true, :space_after_headers => true)
     sanitize markdown.render(text.to_s)
   end
+
+  def link_to_person(user)
+    link_to user.name, person_path(:name => user)
+  end
+
+  def link_avatar_to_person(user, options = {})
+    options[:size] ||= 48
+    link_to image_tag(user.gravatar_url(:size => options[:size])), person_path(:name => user)
+  end
 end
