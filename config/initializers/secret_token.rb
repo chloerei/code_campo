1db19fdbd32a1b2b5ab1c470f2e75fdb349e554d
Rei <chloerei@gmail.com>
2011-12-02 01:37:43 +0800

    Move secret token to app_config.yml

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CodeCampo::Application.config.secret_token = '55eae02566fdd7adb439b51e241b9229d5d9dd11a415b2590825b9ba243ea2ca391b1723f386834793cf60967a9262b9216bbb4881cae86dbf9ce5823e3b5d11'
+CodeCampo::Application.config.secret_token = APP_CONFIG['secret_token']
