6a331e1403580966f4e9672889df36f2fc263bd3
Rei <chloerei@gmail.com>
2012-02-19 16:50:09 +0800

    fix person page.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,7 +2,9 @@ class PeopleController < ApplicationController
   before_filter :find_person
 
   def show
-    @topics = @person.topics.order_by([[:created_at, :desc]]).page(1).per(5)
+    @topics = @person.topics.order_by([[:created_at, :desc]]).page(1).per(10)
+    @topics_count = @person.topics.count
+    @replies_count = @person.replies.count
   end
 
   protected
