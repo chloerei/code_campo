0cb28a4a2826fb546cddc75174bbea3db2c98e2b
Rei <chloerei@gmail.com>
2012-03-14 21:46:20 +0800

    Add home mainbar bottom fragment.

--- app/models/fragment.rb
+++ app/models/fragment.rb
@@ -1,7 +1,7 @@
 class Fragment
   include Mongoid::Document
 
-  FIELDS = %w(home_sidebar_bottom topics_sidebar_bottom footer)
+  FIELDS = %w(home_mainbar_bottom home_sidebar_bottom topics_sidebar_bottom footer)
   FIELDS.each do |name|
     field name
   end
