f425f8bf40f2bd507b1e45269765eef8490c17c2
Rei <chloerei@gmail.com>
2012-05-02 22:07:45 +0800

    add per_page count.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,6 @@
 class HomepageController < ApplicationController
   def index
-    @topics = Topic.active.limit(10)
+    @topics = Topic.active.limit(15)
     @users_count = User.count
     @topics_count = Topic.count
     @replies_count = Reply.count
--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,5 +1,5 @@
 Kaminari.configure do |config|
-  config.default_per_page = 15
+  config.default_per_page = 20
   config.window = 2
   config.outer_window = 1
   # config.left = 0
